012765b67dc0fb88b795420fdac53375d0fbff83
Paulina Kruszyna <kruszyna.paulina@gmail.com>
2018-01-22 19:23:01 +0100

    Add plugins and server

--- webpack.config.js
+++ webpack.config.js
@@ -4,23 +4,33 @@ const webpack = require('webpack');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeJsPlugin = require('optimize-js-plugin');
 
+var env = process.env.NODE_ENV || 'development';
+
+var plugins = [
+	new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        filename: 'index.html',
+        inject: 'body',
+    })
+];
+
+console.log('NODE_ENV:', env);
+
+if (env === 'production') {
+plugins.push(
+    new webpack.optimize.UglifyJsPlugin(),
+    new OptimizeJsPlugin({
+      sourceMap: false
+    })
+  );
+}
+
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'app.bundle.js'
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'src/index.html', 
-            filename: 'index.html', 
-            inject: 'body'
-        }),
-        new webpack.optimize.UglifyJsPlugin(),
-        new OptimizeJsPlugin({
-            sourceMap: false
-        })
-    ],
     module: {
         rules: [
             {
@@ -40,5 +50,6 @@ module.exports = {
                 ]
             }
         ]
-    }
+    },
+    plugins: plugins
 };
